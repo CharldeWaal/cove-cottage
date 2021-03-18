218d8f6089a84ef6a8f8a35fae49679d91431709
Charl de Waal <charldewaal8@gmail.com>
2021-03-18 21:58:07 +0200

    Add showSlide function

--- script.js
+++ script.js
@@ -6,4 +6,28 @@ let slides = n => {
   showSlides(slideIndex += n);
 }
 
-//Thumbnail image control
+//Thumbnail image controls
+let currentSlide = n => {
+  showSlides(n);
+}
+
+let showSlides = n => {
+  var i;
+  var slides = document.getElementsByClassName("image-slide");
+  var dots = document.getElementsByClassName("dot");
+
+  if(n > slides.length){
+    slideIndex = 1;
+  }
+
+  for(i = 0; i < slides.length; i++){
+    slides[i].style.display = "none";
+  }
+
+  for(i = 0; dots.length; i++){
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
