9bb20ddc6d9e23142b532c4836678090d73e98c2
Charl de Waal <charldewaal8@gmail.com>
2021-03-19 10:43:02 +0200

    Add scroll to top button

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+scrollButton = document.getElementById("scrollBtn");
 
-//Next/previous slide
-function incSlides(n) {
-  showSlides(slideIndex += n);
+window.onscroll = function(){
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    scrollButton.style.display = "block";
+  }
+  else {
+    scrollButton.style.display = "none";
+  }
 }
 
-//Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("image-slide");
-  var dots = document.getElementsByClassName("dot");
-
-  if(n > slides.length){
-    slideIndex = 1;
-  }
-
-  if(n < 1) {
-    slideIndex = slides.length;
-  }
-
-  for(i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  for(i = 0; dots.length; i++){
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
