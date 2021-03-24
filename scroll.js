08c8b22f2e4d9f83c4930d352064af603b914296
Charl de Waal <charldewaal8@gmail.com>
2021-03-24 23:47:00 +0200

    Add horizontal scrolling

--- scroll.js
+++ scroll.js
@@ -1,19 +1,21 @@
-scrollButton = document.getElementById("scrollBtn");
+//variables
+const space = document.querySelector('.space');
+const scrollable = document.querySelector('.scrollable');
+space.style.height = `${calcDynamicHeight(scrollable)}px`;
 
-window.onscroll = function(){
-  scrollFunction()
-};
-
-function scrollFunction() {
-  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-    scrollButton.style.display = "block";
-  }
-  else {
-    scrollButton.style.display = "none";
-  }
+function calcDynamicHeight(ref) {
+  const vw = window.innerWidth;
+  const vh = window.innerHeight;
+  const objectWidth = ref.scrollWidth;
+  console.log(objectWidth);
+  return objectWidth - vw + vh + 100; // 150 is the padding (in pixels) desired on the right side of the .cards container. This can be set to whatever your styles dictate
 }
 
-function scrollToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+window.addEventListener('scroll', () => {
+  const sticky = document.querySelector('.sticky');
+  scrollable.style.transform = `translateX(-${sticky.offsetTop}px)`;
+});
+
+window.addEventListener('resize', () => {
+  space.style.height = `${calcDynamicHeight(scrollable)}px`;
+});
