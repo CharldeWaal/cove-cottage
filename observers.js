ee77ba1a66b4532a64425bb517f4440aa8a41722
Charl de Waal <charldewaal8@gmail.com>
2021-04-13 21:33:12 +0200

    Add fade-in

---
+++ observers.js
@@ -0,0 +1,25 @@
+const fadeins = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(entries, appearOnScroll)
+{
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return;
+        }
+        else{
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, 
+appearOptions);
+
+fadeins.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
